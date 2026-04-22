2ec08b31333f0c511ed8c8074ab091e4c536a0c2
bhavesh joshi <joshbhavesh10@gmail.com>
2026-04-22 23:42:57 +0530

    errors fixed

--- backend/index.ts
+++ backend/index.ts
@@ -7,7 +7,7 @@ import apiRoutes from './routes/api';
 dotenv.config();
 
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT:any = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
@@ -28,7 +28,7 @@ const connectDB = async () => {
 };
 
 connectDB().then(() => {
-    app.listen(PORT, () => {
-        console.log(`Server running on port ${PORT}`);
-    });
+app.listen(PORT, "0.0.0.0", () => {
+    console.log(`Server running on port ${PORT}`);
+});
 });
